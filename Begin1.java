c80e69b9d27a7fef43a0f885f8935c1092312569
zhi <1061863828@qq.com>
2018-02-21 01:14:21 +0800

    git test

---
+++ Begin1.java
@@ -0,0 +1,10 @@
+public class Begin1{
+	/* 这是主方法 */
+	public static void main(String args[]){
+		say(); //主方法调用子方法
+	}
+	/* 这是子方法 */
+	public static void say(){ //void表示没有返回值
+		System.out.println("我要学习Java"); //此方法输出一个字符串
+	}
+}
